691c699492205a99a4e4147323e26cec9f92ac71
yonatanp <yonatan.perry@gmail.com>
2017-01-27 17:02:00 +0200

    port argument for localbackdoor

--- tornado_backdoor/localbackdoor.py
+++ tornado_backdoor/localbackdoor.py
@@ -1,4 +1,4 @@
-
+import sys
 import signal
 
 from tornado.ioloop import IOLoop
@@ -10,8 +10,13 @@ def handle_SIGINT(signum, frame):
     io_loop.add_callback(io_loop.stop)
 
 if __name__ == '__main__':
+    if sys.argv[1:]:
+        port = int(sys.argv[1])
+    else:
+        port = 1234
+    print "Interpreter listening for TCP connections on port %s..." % port
     signal.signal(signal.SIGINT, handle_SIGINT)
     server = BackdoorServer()
-    server.listen(1234)
+    server.listen(port)
     IOLoop.instance().start()
 
